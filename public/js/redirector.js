1b7cc6026fd02691cc63f416ca11c6ddf9b324a6
davi-domingues <davi.domingues@sptech.school>
2025-05-15 21:18:05 -0300

    Criando funções de redirecionamento pelas páginas no perfil

---
+++ public/js/redirector.js
@@ -0,0 +1,30 @@
+const homeLink = document.getElementById('homeLink');
+const diarioLink = document.getElementById('diarioLink');
+const dashboardLink = document.getElementById('dashboardLink');
+const calculadoraLink = document.getElementById('calculadoraLink');
+const relogioLink = document.getElementById('relogioLink');
+const configLink = document.getElementById('configLink');
+const sairLink = document.getElementById('sairLink');
+const diarioCard = document.getElementById('diarioCard');
+const dashboardCard = document.getElementById('dashboardCard');
+const calculadoraCard = document.getElementById('calculadoraCard');
+const relogioCard = document.getElementById('relogioCard');
+
+homeLink.addEventListener("click", () => { window.location = "./home.html" });
+diarioLink.addEventListener("click", () => { window.location = "./diario.html" });
+dashboardLink.addEventListener("click", () => { window.location = "./dashboard.html" });
+calculadoraLink.addEventListener("click", () => { window.location = "./calculadora.html" });
+relogioLink.addEventListener("click", () => { window.location = "./sessao.html" });
+configLink.addEventListener("click", () => { window.location = "./config.html" });
+sairLink.addEventListener("click", () => { sessionStorage.clear(); window.location = "../index.html" });
+diarioCard.addEventListener("click", () => { window.location = "./diario.html" });
+dashboardCard.addEventListener("click", () => { window.location = "./diario.html" });
+calculadoraCard.addEventListener("click", () => { window.location = "./diario.html" });
+relogioCard.addEventListener("click", () => { window.location = "./diario.html" });
+
+// window.onload = function testSession() {
+//     if (sessionStorage.length < 2) {
+//         alert("Logue-se para acessar sua página de perfil");
+//         window.location = "../index.html"
+//     }
+// }
