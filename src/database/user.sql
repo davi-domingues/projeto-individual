67fc9d9db20ae9e2dd957f23d0b15a057231b893
davi-domingues <davi.domingues@sptech.school>
2025-05-27 20:15:40 -0300

    Melhorando criação do usuario da api no banco

--- src/database/user.sql
+++ src/database/user.sql
@@ -1,4 +1,4 @@
 DROP USER IF EXISTS webdataviz;
-CREATE USER 'webdataviz'@'%' IDENTIFIED BY 'xbU0VkM5';
+CREATE USER 'webdataviz'@'%' IDENTIFIED BY '#xbU0$VkM5';
 GRANT ALL PRIVILEGES ON appLerSaber.* TO 'webdataviz'@'%';
 FLUSH PRIVILEGES;
