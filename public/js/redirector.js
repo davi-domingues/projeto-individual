2c14b8ac7e83d86fe6536590390fcca39fd92793
davi-domingues <davi.domingues@sptech.school>
2025-06-02 00:50:45 -0300

    Ajuste nas funções de redirecionamento

--- public/js/redirector.js
+++ public/js/redirector.js
@@ -1,7 +1,7 @@
 const homeLink = document.getElementById('homeLink');
 const diarioLink = document.getElementById('diarioLink');
 const dashboardLink = document.getElementById('dashboardLink');
-const calculadoraLink = document.getElementById('calculadoraLink');
+const forumLink = document.getElementById('forumLink');
 const relogioLink = document.getElementById('relogioLink');
 const configLink = document.getElementById('configLink');
 const sairLink = document.getElementById('sairLink');
@@ -9,7 +9,7 @@ const sairLink = document.getElementById('sairLink');
 homeLink.addEventListener("click", () => { window.location = "./index.html" });
 diarioLink.addEventListener("click", () => { window.location = "./diario.html" });
 dashboardLink.addEventListener("click", () => { window.location = "./dashboard.html" });
-calculadoraLink.addEventListener("click", () => { window.location = "./calculadora.html" });
+forumLink.addEventListener("click", () => { window.location = "./forum.html" });
 relogioLink.addEventListener("click", () => { window.location = "./sessao.html" });
 configLink.addEventListener("click", () => { window.location = "./config.html" });
 sairLink.addEventListener("click", () => { sessionStorage.clear(); window.location = "../index.html" });
