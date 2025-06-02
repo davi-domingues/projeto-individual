330271ab5c52d78189270dddf69528f1c2e15d61
davi-domingues <davi.domingues@sptech.school>
2025-06-02 00:51:18 -0300

    Criando views utilizadas no dashboard e outras consultas

--- src/database/structure.sql
+++ src/database/structure.sql
@@ -73,3 +73,59 @@ CREATE VIEW vw_usuario AS
         at_senha             AS senha
 	FROM
 		tb_usuario_leitor;
+        
+CREATE VIEW vw_livro AS
+	SELECT
+        at_fk_idUsuario    AS idUsuario,
+		at_idLivro         AS idLivro,
+        at_nome            AS nome,
+        at_autor           AS autor,
+        at_paginas_total   AS paginasTotal,
+        at_ano             AS ano,
+        at_editora         AS editora,
+        at_genero          AS genero,
+        at_isbn            AS isbn,
+        at_sinopse         AS sinopse
+    FROM
+        tb_livro_individual;
+
+CREATE VIEW vw_leitura AS
+    SELECT
+        at_fk_idUsuario   AS idUsuario,
+        at_fk_idLivro     AS idLivro,
+        at_idLeitura      AS idLeitura,
+        at_paginas_lidas  AS paginasLidas,
+        at_data_registro  AS dtRegistro,
+        at_comentario     AS comentario
+    FROM 
+        tb_leitura_diario;
+
+CREATE VIEW vw_sessao AS
+    SELECT 
+        at_idSessao          AS idSessao,
+        at_fk_idUsuario      AS idUsuario,
+        at_dataRegistro      AS dtRegistro,
+        at_tempo_acumulado   AS tempo
+    FROM
+        tb_sessao_concentracao;
+
+CREATE VIEW vw_status_livro_leitura AS
+    SELECT
+        livro.at_fk_idUsuario    AS idUsuario,
+        livro.at_idLivro         AS idLivro,
+        livro.at_nome            AS nome,
+        livro.at_autor           AS autor,
+        livro.at_paginas_total   AS paginasTotal,
+        (SUM(leitura.at_paginas_lidas)/livro.at_paginas_total)*100 AS porcentagemLida,
+        (CASE
+			WHEN (SUM(leitura.at_paginas_lidas)/livro.at_paginas_total)*100 >= 100 THEN 'Concluído'
+			WHEN (SUM(leitura.at_paginas_lidas)/livro.at_paginas_total)*100 = 0 THEN 'A começar'
+            ELSE 'Em andamento'
+		END) AS status
+    FROM 
+        tb_livro_individual AS livro
+    JOIN
+        tb_leitura_diario AS leitura
+    ON 
+        (livro.at_fk_idUsuario, livro.at_idLivro) = (leitura.at_fk_idUsuario, at_fk_idLivro)
+    GROUP BY 1, 2;
