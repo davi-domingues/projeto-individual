d0536e92ea88b45e3ba4509c9fb549414b323081
davi-domingues <davi.domingues@sptech.school>
2025-06-11 10:01:02 -0300

    Concertando edição de comentário, aplicando scroll automático no ranking e alterando página de configurações para sugestões

--- public/js/redirector.js
+++ public/js/redirector.js
@@ -3,7 +3,7 @@ const diarioLink = document.getElementById('diarioLink');
 const dashboardLink = document.getElementById('dashboardLink');
 const forumLink = document.getElementById('forumLink');
 const relogioLink = document.getElementById('relogioLink');
-const configLink = document.getElementById('configLink');
+const sugestoesLink = document.getElementById('sugestoesLink');
 const sairLink = document.getElementById('sairLink');
 
 homeLink.addEventListener("click", () => { window.location = "./index.html" });
@@ -11,5 +11,5 @@ diarioLink.addEventListener("click", () => { window.location = "./diario.html" }
 dashboardLink.addEventListener("click", () => { window.location = "./dashboard.html" });
 forumLink.addEventListener("click", () => { window.location = "./forum.html" });
 relogioLink.addEventListener("click", () => { window.location = "./sessao.html" });
-configLink.addEventListener("click", () => { window.location = "./config.html" });
+sugestoesLink.addEventListener("click", () => { window.location = "./sugestoes.html" });
 sairLink.addEventListener("click", () => { sessionStorage.clear(); window.location = "../index.html" });
